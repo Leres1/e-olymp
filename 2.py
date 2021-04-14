e53de20a4ba5cacce86de49ae0adb806bc761f4e
Leres1 <volodyakisilv@gmail.com>
2021-04-14 19:04:56 +0300

    the second commit for verification

---
+++ 2.py
@@ -0,0 +1,14 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# Find the number of digits in a nonnegative integer n.
+#
+# Input
+# One nonnegative integer n (0 ≤ n ≤ 2 * 109).
+#
+# Output
+# Print the number of digits in number n.
+# ----------------------------------------------------------------------------------------------------------------------
+def func2(num):
+    print(len(str(num)))
+
+
+func2(input())
